36970c4195a220a7c011ca10eea26fc257f4b7ec
Илья Петров <ip-media@yandex.ru>
2025-04-03 15:24:23 +0300

    mobile menu

--- app/js/main.js
+++ app/js/main.js
@@ -11,3 +11,16 @@ const swiper = new Swiper('.projects__slider', {
         prevEl: '.swiper-button-prev',
     },
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+    $menuBtn = document.querySelector('.header__menu-btn');
+    $openMenuImg = document.querySelector('.header__menu-open');
+    $closeMenuImg = document.querySelector('.header__menu-close');
+    $menu = document.querySelector('.header__menu');
+
+    $menuBtn.addEventListener('click', () => {
+        $menu.classList.toggle('header__menu-hidden');
+        $openMenuImg.classList.toggle('hidden');
+        $closeMenuImg.classList.toggle('hidden');
+    })
+});
